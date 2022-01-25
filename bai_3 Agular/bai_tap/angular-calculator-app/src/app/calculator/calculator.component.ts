cc2fff68c0f72d501e8bced5056de196b144a5e2
C0721G2--L-V-N-Lanh <levanlanh1802@gmail.com>
2022-01-25 07:17:58 +0700

    module5

---
+++ bai_3 Agular/bai_tap/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+public numberOne = 0 ;
+public numberTwo = 0 ;
+public result = 0 ;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  congNum():void{
+    this.result = Number(this.numberOne) + Number(this.numberTwo);
+  }
+  truNum():void{
+    this.result = Number(this.numberOne) - Number(this.numberTwo);
+  }
+  nhanNum():void{
+    this.result = Number(this.numberOne) * Number(this.numberTwo);
+  }
+  chiaNum():void{
+    this.result = Number(this.numberOne) / Number(this.numberTwo);
+  }
+
+}
